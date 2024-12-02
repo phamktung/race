b023991a87bfcb9658f43dcfd280c69ca603b645
phamktung <sundataplus@gmail.com>
2024-12-02 13:35:27 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,11 @@
 /**
- * Internal Dependencies.
- */
-/*import Products from '../src/components/products';*/
-
-/**
  * External Dependencies.
  */
 
-import {getProductsData} from '../src/utils/products';
 import Layout from '../src/components/layout';
 import {getHeaderFooterData} from "../src/utils/layout";
 import {getPosts} from "../src/utils/blog";
 import Posts from "../src/components/posts";
-/*import Pagination from "../src/components/pagination";*/
 
 export default function Home({headerFooter, postsData}) {
   const seo = {
@@ -29,25 +22,19 @@ export default function Home({headerFooter, postsData}) {
   return (
     <Layout headerFooter={headerFooter || {}} seo={seo}>
       <Posts posts={postsData?.posts_data ?? []}/>
-      {/*<Pagination pagesCount={postsData?.page_count ?? 0} postName="blog"/>*/}
-      {/*<Products products={products}/>*/}
+      
     </Layout>
   )
 }
 
 export async function getStaticProps() {
 
-  //const { data: headerFooterData } = await axios.get( HEADER_FOOTER_ENDPOINT );
-  /*const products = await getProductsData();*/
-
   const dataLayout = await getHeaderFooterData();
   const dataPost = await getPosts();
 
-  //console.log('dataPost-----:', dataPost);
   return {
     props: {
       headerFooter: dataLayout?.data ?? {},
-      /*products: products ?? {},*/
       postsData: dataPost ?? {}
     },
 
