1fbbcdd86ca5379b7ec0ef0b83e32b679a456a7f
phamktung <sundataplus@gmail.com>
2025-06-13 16:01:34 +0700

    Update Menu.js

--- src/common/elements/header/Menu.js
+++ src/common/elements/header/Menu.js
@@ -1,51 +1,40 @@
 import {isEmpty} from 'lodash';
 import Link from 'next/link';
 import {getPathNameFromUrl} from "../../../utils/miscellaneous";
+
 const Menu = ({settings}) => {
 
   let headerMenuItems = [];
-  if(settings){
-     headerMenuItems = settings?.header?.headerMenuItems;
+  if (settings) {
+    headerMenuItems = settings?.header?.headerMenuItems;
   }
 
   return (
-    <>
-      <ul>
-        {!isEmpty(headerMenuItems) && headerMenuItems.length ? headerMenuItems.map(menuItem => (
-          <li key={menuItem?.ID}>
-            <Link href={getPathNameFromUrl(menuItem?.url ?? '') || '/'}>
-              {/*<a className="menu-item" dangerouslySetInnerHTML={{__html: menuItem.title}}/>*/}
-              {menuItem.title}
-            </Link>
-            {menuItem.children && menuItem?.children?.length > 0 && (
-              <>
-
-                <ul className={'sub-menu'}>
-                  {menuItem.children.map((childItem) => (
-                    <li key={childItem?.ID}>
-                      <Link  href={getPathNameFromUrl(childItem?.url ?? '') || '/'}>
-                        {/*<a className="menu-item" dangerouslySetInnerHTML={{__html: childItem.title}}/>*/}
-                        {childItem.title}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </>
-            )}
-          </li>
-        )) : null}
-
-
-        <li>
-          <Link href="/account/login">
-            Login
-          </Link></li>
-        <li>
-          <Link href="/account/register">
-            Register
-          </Link></li>
-      </ul>
-    </>
+    <ul className="mainmenu">
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      {!isEmpty(headerMenuItems) && headerMenuItems.length ? headerMenuItems.map(menuItem => (
+        <li key={menuItem?.ID} className={menuItem.children && menuItem?.children?.length > 0 && 'menu-item-has-children'}>
+          <Link href={getPathNameFromUrl(menuItem?.url ?? '', menuItem?.type) || '/'}>
+            {/*<a className="menu-item" dangerouslySetInnerHTML={{__html: menuItem.title}}/>*/}
+            {menuItem.title}
+          </Link>
+          {menuItem.children && menuItem?.children?.length > 0 && (
+            <ul className={'axil-submenu sub-menu'}>
+              {menuItem.children.map((childItem) => (
+                <li key={childItem?.ID}>
+                  <Link href={getPathNameFromUrl(childItem?.url ?? '', childItem?.type) || '/'}>
+                    {/*<a className="menu-item" dangerouslySetInnerHTML={{__html: childItem.title}}/>*/}
+                    {childItem.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </li>
+      )) : null}
+    </ul>
   )
 };
 
