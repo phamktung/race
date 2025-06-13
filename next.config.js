1a93c2105c605a3f2e76dc0c9a189f8d90f89a4c
phamktung <sundataplus@gmail.com>
2025-06-13 14:57:07 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH : "",
-  // images: {
-  //   loader: 'akamai',
-  //   path: process.env.NEXT_PUBLIC_URL,
-  // },
-  images: { domains: ['sukientuanngoc.com'] }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
