5aaf4334e8dc076779015ed0fe928342b6cddc71
phamktung <sundataplus@gmail.com>
2025-06-14 18:36:22 +0700

    Update MobileMenu.jsx

--- src/common/elements/header/MobileMenu.jsx
+++ src/common/elements/header/MobileMenu.jsx
@@ -1,73 +1,72 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link'
-import MenuData from '../../../data/mobilemenu/MenuData.json';
+import {getPathNameFromUrl} from "../../../utils/miscellaneous";
 
-const MobileMenu = ({menuShow, menuHide}) => {
-    if (typeof window !== "undefined") {
-        var colorMode = window.localStorage.getItem('color-mode');
+const MobileMenu = ({menuShow, menuHide, settings}) => {
+
+  let MenuData = [];
+  if (settings) {
+    MenuData = settings?.header?.headerMenuItems;
+  }
+
+  const MenuToggleHandler = (e) => {
+    let selectElm = e.target.nextSibling;
+    if (!selectElm.classList.contains("open")) {
+      selectElm.classList.add("open");
+      e.target.classList.add("open");
+    } else {
+      selectElm.classList.remove("open");
+      e.target.classList.remove("open");
     }
+  }
 
-   const MenuToggleHandler = (e) => {
+  return (
+    <div className={`popup-mobilemenu-area ${menuShow ? "show" : ""}`}>
+      <div className="inner">
+        <div className="mobile-menu-top">
+          <div className="logo">
+            <Link href="/">
+              <Image
+                className="dark-logo"
+                width={141}
+                height={37}
+                src={"/images/logo/amrace-logo.png"}
+                alt="AM Race logo"
+              />
 
-        let selectElm = e.target.nextSibling;
-        if (!selectElm.classList.contains("open")) {
-            selectElm.classList.add("open");
-            e.target.classList.add("open");
-        }else {
-            selectElm.classList.remove("open");
-            e.target.classList.remove("open");
-        }
-   }
-
-    return (
-      <div className={`popup-mobilemenu-area ${menuShow ? "show" : ""}`}>
-        <div className="inner">
-          <div className="mobile-menu-top">
-            <div className="logo">
-                <Link href="/">
-
-                        <Image
-                        className="dark-logo"
-                        width={141}
-                        height={37}
-                        src={"/images/logo/amrace-logo.png"}
-                        alt="AM Race"
-                        />
-
-                </Link>
-            </div>
-            <div className="mobile-close" onClick={menuHide}>
-              <div className="icon">
-                <i className="fal fa-times" />
-              </div>
+            </Link>
+          </div>
+          <div className="mobile-close" onClick={menuHide}>
+            <div className="icon">
+              <i className="fal fa-times"/>
             </div>
           </div>
+        </div>
 
-         <ul className="mainmenu">
-            {MenuData.map((data, index) => (
-                <li className={`${data.submenu ? "menu-item-has-children" :""}`} key={index}>
-                {data.label}
-                {data.submenu ?
+        <ul className="mainmenu">
+          {MenuData.map((data, index) => (
+            <li className={`${data.children && data?.children?.length > 0 && "menu-item-has-children"}`} key={data?.ID}>
+              {data.title}
+              {(data.children && data?.children?.length > 0) ?
                 <ul className="axil-submenu">
-                    {data.submenu.map((submenu, index)=> (
-                        <li key={index}>
-                            <Link href={submenu.subpath}>
-
+                  {data.children.map((childItem) => (
+                    <li key={childItem?.ID}>
+                      <Link href={getPathNameFromUrl(childItem?.url ?? '', childItem?.type) || '/'}>
                                     <span className="hover-flip-item">
-                                        <span data-text={submenu.sublabel}>{submenu.sublabel}</span>
+                                        {childItem.title}
                                     </span>
 
-                            </Link>
-                        </li>
-                      ))}
-                </ul> : "" }
-             </li>
-            ))}
+                      </Link>
+                    </li>
+                  ))}
+                </ul> : ""}
+            </li>
+          ))}
         </ul>
-        </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default MobileMenu;
