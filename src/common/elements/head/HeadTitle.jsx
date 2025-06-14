2ff196f5b0859bec64cd177e2451f97b08579e20
phamktung <sundataplus@gmail.com>
2025-06-14 08:48:51 +0700

    Update HeadTitle.jsx

--- src/common/elements/head/HeadTitle.jsx
+++ src/common/elements/head/HeadTitle.jsx
@@ -5,11 +5,11 @@ const HeadTitle = ({pageTitle}) => {
         <Head>
             <meta charSet="utf-8" />
             <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-            <title>{`${pageTitle} || Blogar - Personal Blog Next JS Template`}</title>
+            <title>{`${pageTitle} || AM Race`}</title>
             <meta name="robots" content="noindex, follow" />
-            <meta name="description" content="Personal Blog Next JS Template" />
+            <meta name="description" content="AM Race - Tổ chức giải chạy bộ cho các tổ chức, doanh nghiệp. Cung cấp tin tức sự kiện, kiến thức thể thao nhằm phát triến sức khỏe cộng đồng." />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-            <link rel="icon" type="image/x-icon" href={`${process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH ?? '' : ''}/favicon.ico`} />
+            <link rel="icon" type="image/x-icon" href={`${process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH ?? '' : ''}/favicon-amrace.ico`} />
         </Head>
      );
 }
