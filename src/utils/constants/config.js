bfc994573b983269f12efcdb12cb427ee346ce5c
phamktung <sundataplus@gmail.com>
2025-06-26 13:35:57 +0700

    Update config.js

--- src/utils/constants/config.js
+++ src/utils/constants/config.js
@@ -3,5 +3,5 @@ export const STRAVA_CLIENT_SECRET = "31a8b54477a6d3d1ddca33f08107c3c7300e540d";
 export const BE_WORDPRESS_SITE_URL = "https://sukientuanngoc.com/api";
 export const PUBLIC_SITE_URL = "https://amrace.com";
 export const WOOCOMMERCE_API_URL = "https://sukientuanngoc.com/api/wp-json/wc/v3";
-export const WOOCOMMERCE_CONSUMER_KEY = "ck_b485bbebfcaecda246991192932c1ee8de90aba1";
-export const WOOCOMMERCE_CONSUMER_SECRET = "cs_33be722b7501d3aae08a73bc8297831f5318df54";
+export const WOOCOMMERCE_CONSUMER_KEY = "ck_1d2f86b6d9d97647355ec466e477b29bd0f9a441";
+export const WOOCOMMERCE_CONSUMER_SECRET = "cs_b2b9c50d8429d011a713db1df9968fabdb345cf5";
